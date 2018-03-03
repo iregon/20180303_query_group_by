f4a79dc608eb0448cf74a4f1a76e16d84fce747f
iregon <0iregon0@gmail.com>
2018-03-03 11:42:00 +0100

    es 2

--- query.sql
+++ query.sql
@@ -6,3 +6,13 @@ SELECT materia, COUNT(*) AS nValutazioni
 FROM valutazioni
 GROUP BY (materia)
 ORDER BY materia
+
+/*
+Per ogni diversa materia calcola la media dei voti visualizzando in ordine decrescente l'elenco
+delle materie con il corrispondente valore calcolato
+*/
+SELECT materia, avg(voto) as mediaVoti
+FROM valutazioni
+GROUP BY (materia)
+ORDER BY AVG(voto) DESC
+
